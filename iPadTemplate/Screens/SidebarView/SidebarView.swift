7471627bddc05ffeadbf3977aeb04f7c037b07b6
Steven barnett <steve@sabarnett.co.uk>
2026-07-18 11:11:39 +0100

    Improve the sample data in the sidebar to give us a more comprehensive example of a list with sections, headers and footers.

--- iPadTemplate/Screens/SidebarView/SidebarView.swift
+++ iPadTemplate/Screens/SidebarView/SidebarView.swift
@@ -14,22 +14,30 @@ import SwiftUI
 struct SidebarView: View {
   @Binding var selection: String?
   @State private var listContent = [
-    "Item 1",
-    "Item 2",
-    "Item 3"
+    "Section 1": ["Item 1", "Item 2", "Item 3"],
+    "Section 2": ["Item 4", "Item 5"],
+    "Section 3": ["Item 6", "Item 7", "Item 8"]
   ]
 
   var body: some View {
     NavigationStack {
       List(selection: $selection) {
-        ForEach(listContent, id: \.self) { item in
-          Section(item) {
-            Text(item)
-              .tag(item)
-          }
+        ForEach(listContent.keys.sorted(), id: \.self) { itemKey in
+          Section(content: {
+            let items = listContent[itemKey] ?? []
+            ForEach(items, id: \.self) { item in
+              Text(item)
+                .tag("\(itemKey) - \(item)")
+                .listRowSeparator(.hidden)
+            }
+          }, header: {
+            Text(itemKey).font(.title2).bold()
+          }, footer: {
+            Text("Helper text for section \(itemKey).")
+          })
         }
       }
-      .listStyle(.plain)
+      .listStyle(.insetGrouped)
       .navigationTitle("Sidebar")
     }
   }
